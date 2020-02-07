80711b920139690ec2a0d446ce628dce90768bb9
nakamura.hiroshi <nakamura.hiroshi@pocke.co.jp>
2020-02-07 11:57:49 +0900

    検索窓つけた。

--- lib/main.dart
+++ lib/main.dart
@@ -27,40 +27,73 @@ class HomeWidget extends StatefulWidget {
 }
 
 class ListState extends State<HomeWidget> {
+  TextEditingController editingController = TextEditingController();
   List listItem;
 
-  Future getData() async{
-    http.Response response = await http.get("https://qiita.com/api/v2/items?page=1&per_page=20&query=flutter");
+  Future getData(String query) async{
+    if (query.isEmpty) {
+      query = 'flutter';
+    }
+    http.Response response = await http.get("https://qiita.com/api/v2/items?page=1&per_page=20&query=" + query);
 
     this.setState((){
       listItem = json.decode(response.body);
     });
   }
+
   @override
   void initState() {
     super.initState();
-    this.getData();
+    this.getData('');
   }
+
+  void filterSearchResults(String query) {
+    this.getData(query);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Qiita App'),),
-      body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              decoration: BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(color: Colors.black38),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onSubmitted: (value) {
+                  filterSearchResults(value);
+                },
+                controller: editingController,
+                decoration: InputDecoration(
+                  labelText: "Search",
+                  hintText: "記事を検索",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+                  ),
+                ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(color: Colors.black38),
+                          ),
+                        ),
+                        child: ListTile(
+                            title: Text(listItem[index]["title"]),
+                            onTap: () {
+
+                            }
+                        ));},
+                  itemCount: listItem == null ? 0: listItem.length,
                 ),
               ),
-              child: ListTile(
-                title: Text(listItem[index]["title"]),
-                onTap: () {
-
-                }
-            ));},
-          itemCount: listItem == null ? 0: listItem.length,
-      ),
+          ],
+        )
+      )
     );
   }
 }
